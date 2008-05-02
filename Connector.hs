d3f3ea1f60e65c30198ee27ae3408e18dbee0b6a
Martijn <Martijn@0806f412-c3e3-11dd-ba5c-d3b7c2b3b534>
2008-05-02 07:09:51 +0000

    Wrote hGetImpatientLine.

--- Connector.hs
+++ Connector.hs
@@ -5,15 +5,12 @@ import System.IO
 import Network
 import Control.Concurrent
 
-hInteract :: Handle -> (String -> String) -> IO ()
-hInteract h f = hGetContents h >>= hPutStr h . f
-
 connect :: String -> Int -> MUD () -> IO ()
 connect host port mud = do
   putStrLn $ "Connecting to " ++ host ++ " port " ++ show port ++ "..."
   conn <- connectTo host (PortNumber (fromIntegral port))  
   vState <- newMVar (fst $ runMUD mud initState)
-  forkIO $ handleSide (hGetLine conn) vState Local conn
+  forkIO $ handleSide (hGetImpatientLine conn 50) vState Local conn
   handleSide getLine vState Remote conn
 
 handleSide :: IO String -> MVar State -> Channel -> Handle -> IO ()
@@ -37,3 +34,15 @@ executeCommand h (Send ch msg) = do
   case ch of
     Local  -> putStrLn msg
     Remote -> do hPutStrLn h msg; hFlush h
+
+hGetImpatientLine :: Handle -> Int -> IO String
+hGetImpatientLine h patience = rec where
+  rec = do
+    c <- hGetChar h
+    if c == '\n'
+      then return [c]
+      else do
+        b <- hWaitForInput h patience
+        if b
+          then rec >>= return . (c:)
+          else return [c]
