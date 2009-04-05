fef4a7324f3f591f79a68479a9e995a82193c3d6
MedeaMelana <MedeaMelana@0806f412-c3e3-11dd-ba5c-d3b7c2b3b534>
2009-04-05 20:00:17 +0000

    Moved executable to its own project.

--- trunk/Yogurt/Network/Yogurt.hs
+++ trunk/Yogurt/Network/Yogurt.hs
@@ -3,10 +3,8 @@ module Network.Yogurt (
   module Network.Yogurt.Mud,
   module Network.Yogurt.Session,
   module Network.Yogurt.Utils,
-  module Network.Yogurt.Engine,
   ) where
 
 import Network.Yogurt.Mud
 import Network.Yogurt.Session
 import Network.Yogurt.Utils
-import Network.Yogurt.Engine
--- trunk/Yogurt/Network/Yogurt/Session.hs
+++ trunk/Yogurt/Network/Yogurt/Session.hs
@@ -14,12 +14,11 @@
 -- 
 -- A module is free to define multiple sessions, in which case you will have to tell @yogurt@ which session to load.
 module Network.Yogurt.Session (
-    Session(..), Reload, session, loadPlugin
+    Session(..), Reload, session
   ) where
 
 import Network.Yogurt.Mud
 import Data.Generics (Typeable)
-import Language.Haskell.Interpreter
 import Control.Monad
 import Data.List (elemIndices)
 
@@ -45,23 +44,3 @@ session = Session
   , portNumber  = error "session portNumber not set"
   , mudProgram  = const (return ())
   }
-
--- | Given a module name, yields all sessions and their names defined in that module.
-loadPlugin :: String -> IO (Either InterpreterError [(String, Session)])
-loadPlugin mn = runInterpreter $ do
-  let moduleName = case elemIndices '.' mn of
-        []  -> mn
-        is  -> take (last is) mn
-  loadModules [moduleName]
-  loadedModuleNames <- getLoadedModules
-  if not (moduleName `elem` loadedModuleNames)
-    then do
-      fail "The module's name must match the filename."
-    else do
-      setImports ["Network.Yogurt.Session", moduleName]
-      symbols <- map name `liftM` getModuleExports moduleName
-      typedSymbols <- mapM (\s -> (,) `liftM` return s `ap` typeOf s) symbols
-      let sessionNames = [ n | (n, t) <- typedSymbols, t == "Session" ]
-      forM sessionNames $ \sn -> do
-        session <- interpret sn (as :: Session)
-        return (sn, session)
